814458c540b7fe455d380dbe211c2c225059475e
Nijil-John <nijiljohn0804@gmail.com>
2025-01-02 10:28:55 +0530

    mongose update

--- config/Database.js
+++ config/Database.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 
 const connectDb = async()=>{
     try {
-        mongoose.connect("mongodb://127.0.0.1:27017/eCommerce")
+        mongoose.connect("mongodb+srv://nijiljohn0804:M1r5DrxqtnTEonMo@cluster1.ztd58.mongodb.net/smartsphere")
     } catch (error) {
         console.log("error in db connection "+error.message);
     }
